903bc5b9c0d7a5e8709a7ae5e70699253f820fa9
Karol Sztajerwald <ksztajerwald@motoinsight.com>
2022-02-11 15:04:50 +0100

    Rewriting cache middleware

--- django_mobile/cache/middleware.py
+++ django_mobile/cache/middleware.py
@@ -1,22 +1,15 @@
 from django.utils.cache import patch_vary_headers
+from django.utils.deprecation import MiddlewareMixin
+
 from django_mobile import get_flavour, _set_request_header
 
 
-class FetchFromCacheFlavourMiddleware:
-    def __init__(self, get_response):
-        self.get_response = get_response
-
-    def __call__(self, request):
+class FetchFromCacheFlavourMiddleware(MiddlewareMixin):
+    def process_request(self, request):
         _set_request_header(request, get_flavour(request))
-        response = self.get_response(request)
-        return response
 
 
-class UpdateCacheFlavourMiddleware:
-    def __init__(self, get_response):
-        self.get_response = get_response
-
-    def __call__(self, request):
-        response = self.get_response(request)
+class UpdateCacheFlavourMiddleware(MiddlewareMixin):
+    def process_response(self, request, response):
         patch_vary_headers(response, ["X-Flavour"])
         return response
